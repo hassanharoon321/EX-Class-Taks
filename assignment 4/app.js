fc2c353b3efd6b0d1c0ca22ad2549e466125974f
hassan haroon <hassanharoon321@gmail.com>
2021-01-26 12:23:29 +0500

    complete 4th assignment

--- assignment 4/app.js
+++ assignment 4/app.js
@@ -1,14 +1,14 @@
 // 1.	Make a program that will filter out all the even numbers
 
-var numbers = [1, 3, 4, 6, 8, 9];
+// var numbers = [1, 3, 4, 6, 8, 9];
 
-function checkEven(num) {
-    if (num % 2 === 0) {
-        console.log(num);
-    }
-}
+// function checkEven(num) {
+//     if (num % 2 === 0) {
+//         console.log(num);
+//     }
+// }
 
-numbers.filter(checkEven);
+// numbers.filter(checkEven);
 
 //---------------------------------------------------------------------------------------
 
@@ -39,8 +39,13 @@ numbers.filter(checkEven);
 // `bykea and uber are good services
 
 var cabs = ["careem", "uber", "bykea"];
-for(var i =0; i<5;i++){
-    console.log(cabs[i]);
+for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+        if (cabs[j] !== cabs[i]) {
+            console.log(cabs[i] + " & " + cabs[j] + " are good services")
+        }
+    }
+
 }
 
 
@@ -56,46 +61,46 @@ for(var i =0; i<5;i++){
 // about its availability.
 
 
-var data = ['cake', 'apple', 'pie', 'cookie', 'chips', 'patties'];
-var userInputCaseSensitive = "CAKE"
-var userInput = userInputCaseSensitive.toLowerCase();
+// var data = ['cake', 'apple', 'pie', 'cookie', 'chips', 'patties'];
+// var userInputCaseSensitive = "CAKE"
+// var userInput = userInputCaseSensitive.toLowerCase();
 
-for(var i =0 ; i<=userInput.length;i++){
-    if(userInput === data[i]){
-        console.log("yes I've");
-        break;
-    }else{
-        console.log("no I don't")
-    }
-}
+// for(var i =0 ; i<=userInput.length;i++){
+//     if(userInput === data[i]){
+//         console.log("yes I've");
+//         break;
+//     }else{
+//         console.log("no I don't")
+//     }
+// }
 
-data.filter((sweets) => {
-    if (sweets === userInput) {
-        console.log(userInput.indexOf());
-    } else {
-        console.log("No I don't");
-    }
-})
+// data.filter((sweets) => {
+//     if (sweets === userInput) {
+//         console.log(userInput.indexOf());
+//     } else {
+//         console.log("No I don't");
+//     }
+// })
 
-var data = ['cake', 'apple', 'pie', 'cookie', 'chips', 'patties'];
-var userInputInsensitive = prompt("Enter Items")
-var userInput = userInputInsensitive.toLowerCase();
-console.log(data.includes(userInput));
+// var data = ['cake', 'apple', 'pie', 'cookie', 'chips', 'patties'];
+// var userInputInsensitive = prompt("Enter Items")
+// var userInput = userInputInsensitive.toLowerCase();
+// console.log(data.includes(userInput));
 
 // 5.	Write a program that takes user input. Convert and show the
 // input in small letters.
 
-var userInput = prompt("Enter Chacter:");
-var lowerCase = userInput.toLocaleLowerCase();
-console.log(lowerCase);
+// var userInput = prompt("Enter Chacter:");
+// var lowerCase = userInput.toLocaleLowerCase();
+// console.log(lowerCase);
 
 // 6.	Write a program to replace the “Hyder” to “Islam” in the word
 // “Hyderabad” and display the result in your browser. Output should be "Islamabad"
 // Hint: replace()
 
-var travel = "Hyder to Islam";
-var travel2= travel.replace("Hyder to Islam","Hyderabad to Islamabad");
-console.log(travel2);
+// var travel = "Hyder to Islam";
+// var travel2= travel.replace("Hyder to Islam","Hyderabad to Islamabad");
+// console.log(travel2);
 
 // 7.	Make following functions
 // i) Sum 
@@ -105,21 +110,21 @@ console.log(travel2);
 
 // Note: Take values from user for passing into arguments (function calling)
 
-function sum(num1, num2) {
-    return console.log(num1 + num2);
-}
-function subract(num1, num2) {
-    return console.log(num1 - num2);
-} 
-function div(num1, num2) {
-    return console.log(num1 / num2);
-} 
-function mul(num1, num2) {
-    return console.log(num1 * num2);
-}
+// function sum(num1, num2) {
+//     return console.log(num1 + num2);
+// }
+// function subract(num1, num2) {
+//     return console.log(num1 - num2);
+// } 
+// function div(num1, num2) {
+//     return console.log(num1 / num2);
+// } 
+// function mul(num1, num2) {
+//     return console.log(num1 * num2);
+// }
 
-sum(2, 3);
-subract(2, 3);
-div(2, 3);
-mul(2, 3);
+// sum(2, 3);
+// subract(2, 3);
+// div(2, 3);
+// mul(2, 3);
 
