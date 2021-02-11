ff86f9e261e2006055a6af106d84c7f5726affdb
hassan haroon <hassanharoon321@gmail.com>
2021-02-11 12:51:32 +0500

    logi changes

--- a minute timer/app.js
+++ a minute timer/app.js
@@ -1,26 +1,44 @@
-// 2) 1 Minute Timer (better to get input from user & make timer according to that)
-
-var hours = document.getElementById("hours").value;
-var minutes = document.getElementById("minutes").value;
-var seconds = document.getElementById("seconds").value;
 var timerShow = document.getElementById("timerShow");
 
-var convertHours = parseInt(hours);
-var convertMinutes = parseInt(minutes);
-var convertSeconds = parseInt(seconds);
-
-function setTime() {
-    timerShow.innerHTML = convertHours + " : " + convertMinutes + " : " + convertSeconds;
+var minutesEl = document.getElementById("minutesEl");
+var minutesValue;
+var minutesInInt;
+function setTimeEl() {
+    minutesValue = minutesEl.value;
+    minutesInInt = Number(minutesValue);
+    timerShow.innerHTML = `${minutesInInt} : 00`;
 }
+
+var straightTime;
+var time;
+var hello;
 
 function start() {
+    straightTime = minutesInInt;
+    time = straightTime * 60;
+    hello = setInterval(function () {
+        var minutes = Math.floor(time / 60);
+        var seconds = time % 60
 
+        seconds = (seconds < 10) ? "0" + seconds : seconds;
+        minutes = (minutes < 10) ? "0" + minutes : minutes;
+        timerShow.innerHTML = `${minutes} : ${seconds}`;
+        time--
+        console.log(minutes)
+        console.log(seconds)
+    }, 1000)
 }
 
+
 function stop() {
+    
+    clearInterval(hello)
 
 }
 
 function reset() {
-    timerShow.innerHTML = "00 : 00 : 00"
+    clearInterval(hello);
+    timerShow.innerHTML = "00 : 00";
 }
+
+
